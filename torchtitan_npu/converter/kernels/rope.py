[fix] Fixed implementation bugs in npu_rope that leads to numerical imprecision
Co-authored-by: mystri<hanboyou@huawei.com>



# message auto-generated for no-merge-commit merge:
!17 merge npu-rope-precision into master

[fix] Fixed implementation bugs in npu_rope that leads to numerical imprecision

Created-by: mystri
Commit-by: mystri
Merged-by: cann-robot
Description: ## 描述
Aligned the following implementation details of npu_rope kernel wrapper to torchtitan's default:
 - Convert to float32 during the rope calculation.
 - Assigned `rotary_mode=interleave` when invoking npu_rotary_mul, since in both torchtitan and our baseline implementation, when doing the rope calculation, the data layout are enforced to interleaved.


## 类型
- [x] Bug 修复
- [ ] 新功能
- [ ] 重构（即不是新增功能，也不是修改bug的代码变动）
- [ ] 构建过程或辅助工具的变动
- [ ] 文档内容更新

## Checklist:
- [ ] 我的代码遵循这个项目的代码风格
- [x] 我已经自己测试过我的代码
- [ ] 我已经更新了相应的文档
- [x] 我已经在标题中正确使用了类型标签（例如：`feat`, `fix`, `refactor`, `docs`, `test`）

## 如何测试
With 1NPU, cropped model, and determinism enabled, verify zero discrepancy of loss values in the first 20 steps:
Benchmark - torchtitan default implementation, no conversion:
![D976DC72-5DBC-4679-D122-210579988569.png](https://raw.gitcode.com/user-images/assets/9028822/0b72b39c-81ae-42d7-88ee-6812312f1421/D976DC72-5DBC-4679-D122-210579988569.png 'D976DC72-5DBC-4679-D122-210579988569.png')
Target Implementation - with npu_rope converter included:
![33C00B89-D7D0-4FAD-D2DE-01E9D3DBEE69.png](https://raw.gitcode.com/user-images/assets/9028822/17c1684c-7e3c-4fa8-b1c8-8db680633012/33C00B89-D7D0-4FAD-D2DE-01E9D3DBEE69.png '33C00B89-D7D0-4FAD-D2DE-01E9D3DBEE69.png')
Ablation experiment - removed Conversion to float32:
![72717E55-C89F-4F6A-EEA3-EA9F4256FA40.png](https://raw.gitcode.com/user-images/assets/9028822/51d38c17-5728-4bb3-b208-3e7978bcb673/72717E55-C89F-4F6A-EEA3-EA9F4256FA40.png '72717E55-C89F-4F6A-EEA3-EA9F4256FA40.png')
Ablation experiment - removed `rotary_mode` assignment:
![image.png](https://raw.gitc

torchtitan_npu/converter/kernels/rope.py
@@ -33,17 +33,20 @@ def _prepare_cos_sin_from_cache(rope_cache: torch.Tensor, x: torch.Tensor):
 
 
 def npu_apply_rotary_emb_deepseek(x: torch.Tensor, freqs_cis: torch.Tensor, interleaved: bool = True) -> torch.Tensor:
+    dtype = x.dtype
     shape = x.shape
+
+    x = x.float()
     if not interleaved:
         x = x.view(*shape[:-1], 2, -1).transpose(-1, -2).contiguous().view(*shape)
 
     cos, sin = _prepare_cos_sin_from_complex(freqs_cis, x.dtype)
-    y = torch_npu.npu_rotary_mul(x, cos, sin)
+    y = torch_npu.npu_rotary_mul(x, cos, sin, rotary_mode='interleave')
 
     if not interleaved:
         y = torch.cat([y[..., 0::2], y[..., 1::2]], dim=-1)
 
-    return y
+    return y.to(dtype)
 
 
 def npu_apply_rotary_emb_llama(xq: torch.Tensor, xk: torch.Tensor, freqs_cis: torch.Tensor) -> Tuple[